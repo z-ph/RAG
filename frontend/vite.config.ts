164fb37204af556e6d1c40f6cb6d9a27b4fcf74a
zph <zphhpzzph@gmail.com>
2026-04-28 21:24:19 +0800

    添加前端路由base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig(({ mode }) => {
   loadNodeEnv(mode);
 
   return {
+    base: process.env.VITE_BASE_ROUTE || "/",
     plugins: [react(), tailwindcss()],
     server: {
       host: "0.0.0.0",
